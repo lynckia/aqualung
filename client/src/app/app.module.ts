e1aa52aa4fc6916dd856d0ea030492594a1f49d4
Álvaro Alonso <aalonsog@dit.upm.es>
2017-05-08 14:02:05 +0200

    Added base component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RoomComponent } from './room/room.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'room', component: RoomComponent
+      } 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
